522b8bc79759ccbf393e38e348a423def6c04e41
Richard Ziegler <rzieglerdevelopment@gmail.com>
2018-03-25 11:34:04 -0500

    Created Forms, Labels, Input Boxes, and Submit Button

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            <label for="rot">Rotate by:</label>
+            <input id="rot" type="text" name="rot" />
+
+
+            <textarea name="text"></textarea>
+
+
+
+            <input type="submit" value="Submit Querey" /> 
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
